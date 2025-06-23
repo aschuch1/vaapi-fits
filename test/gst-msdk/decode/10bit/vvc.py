2e1b0d333b6bd02cb34c1c3e20af574db5b71e14
aschuch1 <ana.schuch@intel.com>
2025-06-23 15:21:01 -0700

    Create vvc tests

---
+++ test/gst-msdk/decode/10bit/vvc.py
@@ -0,0 +1,39 @@
+###
+### Copyright (C) 2018-2020 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from .....lib import *
+from .....lib.gstreamer.msdk.util import *
+from .....lib.gstreamer.msdk.decoder import DecoderTest
+
+spec = load_test_spec("vvc", "decode", "10bit")
+
+@slash.requires(*platform.have_caps("decode", "vvc_10"))
+@slash.requires(*have_gst_element("msdkvvcdec"))
+class default(DecoderTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      # default metric
+      metric      = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0),
+      caps        = platform.get_caps("decode", "vvc_10"),
+      gstdecoder  = "msdkvvcdec",
+      gstparser   = "vvcparse",
+    )
+
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+
+    dxmap = {".ivf" : "ivfparse", ".webm" : "matroskademux", ".obu" : "vvcparse"}
+    ext = os.path.splitext(self.source)[1]
+    dx = dxmap.get(ext, None)
+    assert dx is not None, "Unrecognized source file extension {}".format(ext)
+
+    vars(self).update(
+      case        = case,
+      gstdemuxer  = dx if self.gstparser not in dx else None,
+    )
+    self.decode()
---
+++ test/gst-msdk/decode/vvc.py
@@ -0,0 +1,30 @@
+###
+### Copyright (C) 2018-2019 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.gstreamer.msdk.util import *
+from ....lib.gstreamer.msdk.decoder import DecoderTest
+
+spec = load_test_spec("vvc", "decode")
+
+@slash.requires(*platform.have_caps("decode", "vvc"))
+@slash.requires(*have_gst_element("msdkh266dec"))
+class default(DecoderTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      # default metric
+      metric      = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0),
+      caps        = platform.get_caps("decode", "vvc"),
+      gstdecoder  = "msdkh266dec",
+      gstparser   = "h266parse",
+    )
+
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    vars(self).update(case = case)
+    self.decode()
